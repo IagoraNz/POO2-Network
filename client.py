74bf8a508a8c56b977b00782372ed780f2b5efa8
FrancinaldoSB <francinaldosousabarbosa21@gmail.com>
2024-11-01 13:47:09 -0300

    Quase

--- client.py
+++ client.py
@@ -1,13 +1,46 @@
 import socket
-ip = '192.168.68.164'
-port = 9000
-addr = ((ip,port)) #define a tupla de endereco
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET parametro para informar a familia do protocolo, SOCK_STREAM indica que eh TCP/IP
-client_socket.connect(addr) #realiza a conexao
-while(True):
-    mensagem = input('Digite uma mensagem para enviar ao servidor: ')
-    client_socket.send(mensagem.encode()) #envia mensagem
-    print ('Mensagem enviada!\n')
-    print(client_socket.recv(1024).decode())
+import time
 
-client_socket.close() #fecha a conexao
+ip = 'localhost'
+port = 9000
+addr = (ip, port)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(addr)
+
+def menu():
+    print('1 - Enviar mensagem')
+    print('2 - Escutar mensagem')
+    print('3 - Enviar mensagem e escutar resposta')
+    print('0 - Sair')
+    return input('Escolha uma opcao: ')
+
+def enviar_mensagem():
+    msg = input('Digite a mensagem: ')
+    print('Clientes conectados: 1, 2, 3')
+    id_cliente = input('Digite o id do cliente que vai receber: ')
+    # Formata mensagem como 'id:mensagem'
+    msg = f"{id_cliente}:{msg}"
+    client_socket.send(msg.encode()) 
+
+def escutar_mensagem():
+    mensagem = client_socket.recv(1024).decode()
+    print(f'Mensagem recebida: {mensagem}')
+
+def enviar_e_escutar():
+    enviar_mensagem()
+    escutar_mensagem() 
+
+while True:
+    opcao = menu()
+    if opcao == '1':
+        enviar_mensagem()
+    elif opcao == '2':
+        escutar_mensagem()
+    elif opcao == '3':
+        enviar_e_escutar()
+    elif opcao == '0':
+        break
+    else:
+        print('Opcao invalida')
+
+client_socket.close()
--- server.py
+++ server.py
@@ -1,54 +1,52 @@
 import socket
 
-host = '192.168.68.164'
+host = 'localhost'
 port = 9000
 addr = (host, port)
 
-# Configuração do servidor
 serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serv_socket.bind(addr)
-serv_socket.listen(2)  # Limite de 2 conexões
+serv_socket.listen(3) 
 
 print('Aguardando conexão dos clientes...')
 
-# Aceita as duas conexões
-con1, cliente1 = serv_socket.accept()
-print('Cliente 1 conectado, aguardando Cliente 2...')
-con2, cliente2 = serv_socket.accept()
-print('Cliente 2 conectado!')
+connections = {}
 
-print('Aguardando mensagens...')
+for i in range(1, 4):
+    con, client = serv_socket.accept()
+    connections[i] = con
+    print(f'Cliente {i} conectado')
 
-# Loop principal para alternar entre as conexões
+print('Clientes conectados: 1, 2, 3')
+
 while True:
-    # Recebe a mensagem do Cliente 1 e encaminha para Cliente 2
-    try:
-        recebe1 = con1.recv(1024)
-        if not recebe1:
-            print("Cliente 1 desconectou.")
-            break  # Encerra o loop se Cliente 1 desconectar
-        print('Mensagem recebida de Cliente 1:', recebe1.decode())
-        con1.send('Mensagem recebida'.encode())  # Confirma recebimento a Cliente 1
-        con2.send(recebe1)  # Repassa a mensagem para Cliente 2
-    except ConnectionResetError:
-        print("Cliente 1 desconectou.")
+    for client_id, con in connections.items():
+        try:
+            data = con.recv(1024)
+            if data:
+                decoded_data = data.decode()
+                print(f'Mensagem recebida de {client_id}: {decoded_data}')
+
+                id_cliente, msg = decoded_data.split(':')
+                id_cliente = int(id_cliente)
+                
+                if msg == 'exit':
+                    print(f'Cliente {id_cliente} desconectado')
+                    connections[id_cliente].close()
+                    del connections[id_cliente]
+                    break 
+                else:
+                    if id_cliente in connections:
+                        connections[id_cliente].send(f'{client_id}:{msg}'.encode())
+        except ConnectionResetError:
+            print(f'Cliente {client_id} desconectado abruptamente')
+            con.close()
+            del connections[client_id]
+            break 
+
+    if not connections:
+        print("Todos os clientes se desconectaram.")
         break
 
-    # Recebe a mensagem do Cliente 2 e encaminha para Cliente 1
-    try:
-        recebe2 = con2.recv(1024)
-        if not recebe2:
-            print("Cliente 2 desconectou.")
-            break  # Encerra o loop se Cliente 2 desconectar
-        print('Mensagem recebida de Cliente 2:', recebe2.decode())
-        con2.send('Mensagem recebida'.encode())  # Confirma recebimento a Cliente 2
-        con1.send(recebe2)  # Repassa a mensagem para Cliente 1
-    except ConnectionResetError:
-        print("Cliente 2 desconectou.")
-        break
-
-# Fecha as conexões
-con1.close()
-con2.close()
 serv_socket.close()
